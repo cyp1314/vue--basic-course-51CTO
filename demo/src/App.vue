dd0955d576341b734fa55ea82cf54e16b5cbca88
ynzy <a758922096@gmail.com>
2019-02-28 19:57:35 +0800

    按需引入vant的swipe实现轮播组件

---
+++ demo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+
+
+<van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- demo/src/main.js
+++ demo/src/main.js
@@ -2,18 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 
-import store from './store/index'
+// import Vant from 'vant';
+// import 'vant/lib/index.css';
 
-
+// Vue.use(Vant);
+import './vant-component'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  store,
-  router,
   components: { App },
   template: '<App/>'
 })
